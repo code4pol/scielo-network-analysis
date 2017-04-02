1bffa33835de6f14d3ded599a5b837f56e42cdd1
Alexandre Gomes <alegomes@gmail.com>
2017-04-02 17:11:05 -0300

    incorporação das mudanças feitas com a Tayrine

--- scielo.py
+++ scielo.py
@@ -1,6 +1,9 @@
 import urllib.request
 import re
 import xmltodict
+from xml.parsers.expat import ExpatError
+
+f = open('IDhtml.csv')
 
 def retrieve_data(article_html_url):
 	article_id = re.search("http://.*pid=(.*?)&",article_html_url).group(1)
@@ -10,34 +13,71 @@ def retrieve_data(article_html_url):
 	xml = file.read()
 	file.close()
 
-	return xmltodict.parse(xml)
+	data = {}
+	try:
+		data = xmltodict.parse(xml)
+	except ExpatError as e:
+		# XML com problemas
+		pass	
+
+	return data
 
 
-def get_authors(person):
+def get_linked_authors_names_expanded(person):
 	authors = []
+	
 	if isinstance(person['name'],list):
 		for name in person['name']:
-			authors.append("%s %s" % (name['surname'], name['given-names']))
+
+			# Em http://www.scielo.br/scielo.php?script=sci_arttext&pid=S0103-20702014000200003&lang=pt, vem um name = None
+			if name:
+				surname = ''
+				givennames = ''
+				if 'surname' in name:
+					surname = name['surname']
+				if 'given-names' in name:
+					givennames = name['given-names']
+
+				authors.append("%s %s" % (surname, givennames))
 	else:
 		name = person['name']
-		authors.append("%s %s" % (name['surname'], name['given-names']))
+		if name: # Sim, ha artigos cujo name = None
+			surname = ''
+			givennames = ''
+			if 'surname' in name:
+				surname = name['surname']
+			if 'given-names' in name:
+				givennames = name['given-names']
+			authors.append("%s %s" % (surname, givennames))
 
 	return authors
 
 
 def get_author(data):
-	# E se houver mais de um author?
-	contrib = data['article']['front']['article-meta']['contrib-group']['contrib']
-	if contrib['@contrib-type'] == 'author':
-		author = "%s %s" % (contrib['name']['surname'], contrib['name']['given-names'])
+	authors = []
 
-	return author
+	try:
+		contribs = data['article']['front']['article-meta']['contrib-group']['contrib']
+	except KeyError:
+		return authors
+
+	if isinstance(contribs,list):
+		for contrib in contribs:
+			if contrib['@contrib-type'] == 'author':
+				authors.append("%s %s" % (contrib['name']['surname'], contrib['name']['given-names']))
+	else:
+		contrib = contribs
+		if contrib['@contrib-type'] == 'author':
+			authors.append("%s %s" % (contrib['name']['surname'], contrib['name']['given-names']))
+
+	return authors
 
 
 def get_refs(data):
-	return data['article']['back']['ref-list']
+	if 'back' in data['article']:
+		return data['article']['back']['ref-list']
 
-def get_names(ref):
+def get_linked_authors_names(ref):
 	names = []
 	if 'person-group' in ref['nlm-citation']:
 
@@ -50,41 +90,68 @@ def get_names(ref):
 
 		for person in person_group:
 			if person['@person-group-type'] == 'author':
-				names.extend(get_authors(person))
+				
+				expanded_names = get_linked_authors_names_expanded(person)
+				
+				# Como eh o caso do B9 em http://www.scielo.br/scielo.php?script=sci_arttext&pid=S0104-40362014000400007&lang=pt
+				if len(expanded_names) == 0:
+					if 'collab' in ref['nlm-citation']:
+						expanded_names = [ref['nlm-citation']['collab']]
+
+				names.extend(expanded_names)
 
 	return names
 
-def get_linked_authors(data):
+def get_linked_authors(refs):
 	linked_authors = []
+	
+	if isinstance(refs['ref'],list):
+		for ref in refs['ref']:
+			id = ref['@id']
+			type = ref['nlm-citation']['@citation-type']
 
-	for ref in refs['ref']:
+			if 'article-title' in ref['nlm-citation']:
+				title = ref['nlm-citation']['article-title']['#text']
+			else:
+				title = "?"
+
+			linked = { 'id' : id, 'type' : type, 'names' : get_linked_authors_names(ref)}
+			linked_authors.append(linked)
+	else:
+		ref = refs[ 'ref']
 		id = ref['@id']
 		type = ref['nlm-citation']['@citation-type']
-
-		if 'article-title' in ref['nlm-citation']:
-			title = ref['nlm-citation']['article-title']['#text']
-		else:
-			title = "?"
-
-		linked = { 'id' : id, 'type' : type, 'names' : get_names(ref)}
+		linked = { 'id' : id, 'type' : type, 'names' : get_linked_authors_names(ref)}
 		linked_authors.append(linked)
 
 	return linked_authors
 
 
-def print_csv(author, linked_authors):
-	for linked_author in linked_authors:
-		for linked_name in linked_author['names']:
-			print("%s,%s,%s,%s" % (author,linked_author['id'],linked_author['type'],linked_name))
+def print_csv(authors, linked_authors):
+	for author in authors:
+		for linked_author in linked_authors:
+			for linked_name in linked_author['names']:
+				print("%s,%s,%s,%s" % (author,linked_author['id'],linked_author['type'],linked_name))
 
-
-if __name__ == "__main__":
+def process_article(article_html_url):
 	
-	article_html_url = "http://www.scielo.br/scielo.php?script=sci_arttext&pid=S0104-026X2017000100031&lng=en&nrm=iso"
 	data = retrieve_data(article_html_url)
 
-	author = get_author(data)
-	refs = get_refs(data)
-	linked = get_linked_authors(data)
-	print_csv(author, linked)
+	if data:
+		author = get_author(data)
+		refs = get_refs(data)
 
+		if refs:
+			linked = get_linked_authors(refs)
+			print_csv(author, linked)
+
+if __name__ == "__main__":
+
+	i = 540
+	f = f.readlines()[i-1:]
+
+	for l in f:
+		# l = "http://www.scielo.br/scielo.php?script=sci_arttext&pid=S1982-88372016000200074&lang=pt"
+		print(i,l)
+		process_article(l)
+		i += 1
